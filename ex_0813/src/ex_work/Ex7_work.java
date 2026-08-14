481f4d416570ac636fae7e7addebd41ca751316f
NayunKim715 <jouer6890@naver.com>
2026-08-14 12:32:34 +0900

    0813 수업내용

--- ex_0813/src/ex_work/Ex7_work.java
+++ ex_0813/src/ex_work/Ex7_work.java
@@ -15,11 +15,69 @@ public class Ex7_work {
         System.out.print("홀수 : ");
         int n = sc.nextInt(); 
         
-        int[][] arr = new int[n][n]; 
+        /*int[][] arr = new int[n][n]; 
 
        
             System.out.println();
     
 
+
+            while( num <= size * size ){
+
+              square[y][x] = num;
+        
+              if( num % size == 0 ){
+                 y++;
+              }else{
+                 y--;
+                 x++;
+              }
+
+              if( y < 0 )
+                y = size - 1;
+
+              if( x >= size)
+                x = 0;
+
+
+            }
+
     }//main
+}
+    */
+        // 마방진 작성 로직 - missing code 완성
+        // 가장 대표적인 홀수 마방진(3x3, 5x5...) 만들기 방식
+        int[][] square = new int[n][n];
+        int num = 1;
+        int size = n;
+        int x = size / 2; // 중앙열
+        int y = 0;        // 첫행
+        
+        while (num <= size * size) {
+            square[y][x] = num;
+
+            if (num % size == 0) {
+                y++;
+            } else {
+                y--;
+                x++;
+            }
+
+            if (y < 0)
+                y = size - 1;
+
+            if (x >= size)
+                x = 0;
+
+            num++;
+        }
+
+        // 마방진 출력
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%02d ", square[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
