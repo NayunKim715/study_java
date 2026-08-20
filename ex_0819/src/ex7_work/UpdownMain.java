7904c17aa689287d2b75cbdfe9920578e47044c7
NayunKim715 <jouer6890@naver.com>
2026-08-20 16:49:43 +0900

    0819 수업내용

--- ex_0819/src/ex7_work/UpdownMain.java
+++ ex_0819/src/ex7_work/UpdownMain.java
@@ -1,5 +1,7 @@
 package ex7_work;
 
+import java.util.Scanner;
+
 public class UpdownMain {
    public static void main(String[] args) {
       //1 ~ 50사이의 난수를 발생시키고
@@ -11,6 +13,18 @@ public class UpdownMain {
       //UP
       //정수 : 25
       //3회 안에 정답
+      Scanner sc = new Scanner(System.in);
+      int select = 0;
+      boolean check = false;
+
+      UpdownSub us = new UpdownSub();
+      //정답 맞출 때까지 무한반복 do-while문
+      do{
+         System.out.print("정수 : ");
+         select = sc.nextInt();
+         check = us.check(select);
+
+      }while( check ); // cehck == true
 
    }//main
 }
---
+++ ex_0819/src/ex7_work/UpdownSub.java
@@ -0,0 +1,26 @@
+package ex7_work;
+
+import java.util.Random;
+
+public class UpdownSub {
+    
+    private int random = new Random().nextInt(50) + 1;
+    private int count = 0;
+    private boolean result = true;
+
+    //정답판별 메서드
+    public boolean check( int n ){
+        count++; //몇번만에 정답 맞췄는지
+
+        if( n < random )
+            System.out.println("UP");
+        else if( n > random )
+            System.out.println("DOWN");
+        else{
+            System.out.println(count + "회 만에 정답");
+            result = false;
+        }
+        return result;
+
+    }//check()
+}
