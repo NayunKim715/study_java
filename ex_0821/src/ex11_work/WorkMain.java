a612f2a04112e75cfafce8e9468d4346f0fd3652
NayunKim715 <jouer6890@naver.com>
2026-08-28 17:32:26 +0900

    0821 수업내용

--- ex_0821/src/ex11_work/WorkMain.java
+++ ex_0821/src/ex11_work/WorkMain.java
@@ -1,5 +1,7 @@
 package ex11_work;
 
+import java.util.Scanner;
+
 public class WorkMain {
     public static void main(String[] args) {
         //문제 : PEALP
@@ -7,8 +9,29 @@ public class WorkMain {
         //ealpp오답
         //>>apple
         //apple정답!!
-        String[] strArr = { "APPLE", "ORANGE", "HOPE", "VIEW" };
+        WorkSub ws = new WorkSub();
+        
+        //정답단어
+        String answer = ws.getAnswer();
 
+        //정답을 섞어서 문제로 만들기
+        String question = ws.scrambleWord( answer );
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "문제 : " + question );
+
+        while(true){
+            System.out.print( ">> ");
+            String str = sc.next();
+
+            if( str.equalsIgnoreCase( answer ) ){
+                System.out.println(str + "은 정답입니다");
+                break;
+            }else{
+                System.out.println(str + "은 오답입니다");
+            }
+
+        }//while
 
     }//main
 }
